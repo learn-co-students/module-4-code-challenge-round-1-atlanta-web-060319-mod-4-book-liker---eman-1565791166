b15c602eb204f0a4fb1e1beb36b3d7da178b337f
Nerajno <Nerajno@gmail.com>
2019-08-14 11:42:14 -0400

    final commmit

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -24,7 +24,7 @@ const BotCard = props => {
       <div
         className="ui card"
         key={bot.id}
-        onClick={(event) => console.log(bot)}
+        onClick={(event) => bot.addToArmy(event, bot)}// make the Card  ??/
       >
         <div className="image">
           <img alt="oh no!" src={bot.avatar_url} />
