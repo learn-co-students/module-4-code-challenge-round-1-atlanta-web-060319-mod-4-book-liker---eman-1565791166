4062acea44d28481be7d0050dd2bb96c9b93dd05
Dylan Harris <dylanharris23@gmail.com>
2019-08-14 11:31:54 -0400

    Done

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -9,8 +9,8 @@ class YourBotArmy extends React.Component {
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
             Your Bot Army
+            {this.props.enlistedBots.map((bot, i) => <BotCard key={i} bot={bot} handleMyBots={this.props.handleMyBots}/>)}
           </div>
         </div>
       </div>
