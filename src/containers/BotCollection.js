7c1883f2b87b1f5dc46559953722b50639a28c94
github username <github email address>
2019-08-14 11:34:44 -0400

    hating life

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -3,12 +3,15 @@ import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
   //your code here
+  constructor(props){
+	  super(props)
+  }
 
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {this.props.robots.map(robot => <BotCard bot={robot} handleMyArmy={this.props.handleMyArmy}/>)}
     		  Collection of all bots
     		</div>
   	  </div>
